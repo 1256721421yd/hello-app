37fc8e84d1926b95939e0be21505596843372947
1256721421yd <yd1256721421@163.com>
2018-07-30 20:44:55 +0800

    hello-app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './components/app/App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/App';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
